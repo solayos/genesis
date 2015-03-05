84ac451f87f7258a3aabf8be003f345764657e58
Brendo Marinho <brendonmm@outlook.com>
2015-03-05 11:58:30 -0300

    HTML: - Old mobile viewport - iOS Web App - Android Web App - Windows Phone App/ClearType - Format detection
    CSS:
    - main tag
    - media queries Webkit/Windows Phone
    - orientation
    
    JS:
    - detect orientation
    - change directory of images by DPI

--- base-site/js/scriptGeral.js
+++ base-site/js/scriptGeral.js
@@ -25,5 +25,56 @@
 })();
 
 $(function () {
-
+    
+    /* BASE URL VIA DEVICE PIXEL RATIO - ANDROID */
+    var baseUrl = 'file:///android_asset/www/img-js-diff/ratiores/'+getDensityDirectoryName()+'/'
+    document.write('<base href="'+baseUrl+'">');
 });
+
+/* MUDA DIRETORIO DAS IMAGENS E CONTEUDO 
+    *-- Baseado no Device Pixel Ratio
+ */
+function getDensityDirectoryName() {
+    if(!window.devicePixelRatio) {
+        return 'mdpi';
+    }
+    
+    if(window.devicePixelRatio > 3) {
+        return 'xxxhdpi';
+    } else if(window.devicePixelRatio > 2) {
+        return 'xxhdpi';
+    } else if(window.devicePixelRatio > 1.5) {
+        return 'xhdpi';
+    } else if(window.devicePixelRatio > 1.0) {
+        return 'hdpi';
+    }
+
+    return 'mdpi';
+}
+
+/* VERIFICA ORIENTACAO */
+window.onload = function initialLoad(){  
+	updateOrientation();
+}
+var contentType = 'vertical';
+
+function updateOrientation(){
+	switch(window.orientation){  
+		case 0:  			
+			contentType = "vertical";  
+		break;  
+	
+		case -90:			
+			contentType = "horizontal";  
+		break;  
+	
+		case 90:
+			contentType = "horizontal";  
+		break;  
+	
+		case 180:			
+			contentType = "vertical";  
+		break;  
+	}	
+	document.getElementsByTagName('body')[0].className = contentType;	
+} 
