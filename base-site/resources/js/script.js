426ad437509b596e1ce44a50fc89b21d346576a5
Brendo Marinho <brendonmm@outlook.com>
2015-03-06 00:06:35 -0300

    Bootstrap updated

--- base-site/resources/js/script.js
+++ base-site/resources/js/script.js
@@ -1,3 +1,8 @@
+var larguraJanela = $(window).width(),
+    alturaJanela = $(window).innerHeight(),
+    urlSite = window.location.href,
+    ie;
+
 /* GOOGLE FONTS PT Sans */
 (function() {
     var link_element = document.createElement("link"),
@@ -24,12 +29,102 @@
     s.parentNode.insertBefore(link_element, s);
 })();
 
+$(window).bind('resize', function(e){
+	if (window.RT) clearTimeout(window.RT);
+	window.RT = setTimeout(function() {
+	    // code here
+	}, 100);
+
+});
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+$(window).on('orientationchange', function (event) {
+    updateOrientation();
+});
+
 $(function () {
-    $('[data-toggle="popover"]').popover({delay: { 'show': 200, 'hide': 1000 }});
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $(window).resize(function () {
+        // code here
+    });
+
+    var bind = function (el, event, fn) {
+        if (el.addEventListener) {
+            el.addEventListener(event, fn, false);
+        } else {
+            el.attachEvent('on' + event, fn);
+        }
+    };
+
+    $('[data-toggle="popover"]').popover({ delay: { 'show': 200, 'hide': 200} });
     /* BASE URL VIA DEVICE PIXEL RATIO - ANDROID */
     //var baseUrl = 'file:///android_asset/www/img-js-diff/ratiores/'+getDensityDirectoryName()+'/'
     //document.write('<base href="' + baseUrl + '">');
 });
+
+/* ORIENTATION */
+window.onload = function initialLoad(){  
+	updateOrientation();
+}
+
+var myOrientation = 'vertical';
+
+function updateOrientation(){
+	switch(window.orientation){  
+		case 0:  			
+			myOrientation = "vertical";  
+		break;  
+	
+		case -90:			
+			myOrientation = "horizontal";  
+		break;  
+	
+		case 90:
+			myOrientation = "horizontal";  
+		break;  
+	
+		case 180:			
+			myOrientation = "vertical";  
+		break;  
+	}
+
+	$('body').attr('data-orientation', myOrientation);
+    $('#my-orientation').html(myOrientation);
+	//document.getElementsByTagName('body')[0].className = myOrientation;
+	//document.getElementById('my-orientation').innerHTML = myOrientation;
+} 
+
+
+$.ajax({
+    url: "resources/data/estrutura.json",
+    dataType: "json",
+    success: function(data) {
+        //console.log(data);
+        $.each(data.employees,function(i,emp){
+            $('.json-import').append('<li>'+emp.firstName+' '+emp.lastName+'</li>');
+        });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.log('ERROR', textStatus, errorThrown);
+    }
+});
+
+
 
 /* MUDA DIRETORIO DAS IMAGENS E CONTEUDO 
     *-- Baseado no Device Pixel Ratio
@@ -51,35 +146,6 @@ function getDensityDirectoryName() {
 
     return 'mdpi';
 }
-
-/* VERIFICA ORIENTACAO */
-window.onload = function initialLoad(){  
-	updateOrientation();
-}
-var contentType = 'vertical';
-
-function updateOrientation(){
-	switch(window.orientation){  
-		case 0:  			
-			contentType = "vertical";  
-		break;  
-	
-		case -90:			
-			contentType = "horizontal";  
-		break;  
-	
-		case 90:
-			contentType = "horizontal";  
-		break;  
-	
-		case 180:			
-			contentType = "vertical";  
-		break;  
-	}	
-    
-	document.getElementsByTagName('body')[0].className = contentType;
-	document.getElementById('content-type').innerHTML = contentType;
-} 
 
 //FUNCOES DE COMPARTILHAMENTO
 /* Compartilhar Facebook */
@@ -129,3 +195,127 @@ function addBookmark() {
         alert('Pressione as teclas CTRL + D para adicionar aos favoritos.'); // Chrome, Safari, Firefox 8+, Opera 15+
 	}
 }
+
+/* @begin Plugin de Paginacao Slide */
+
+
+var isHome = true,
+    isMobile = false,
+    position = 0,
+    isScrolling = true,
+    alturaContainerSlide = $('.containerSlide').innerHeight();
+
+/* PAGINACAO DOS SLIDES */
+function animaSlideHome() {
+    enterTop();
+
+    /* ANIMA SLIDE AO FAZER SCROLL */
+    if (ie === 8) {
+        window.attachEvent('onmousewheel', handleMouseWheel);
+    }
+    else {
+        window.addEventListener('DOMMouseScroll', handleMouseWheel, false);
+        window.addEventListener('mousewheel', handleMouseWheel, false);
+    }
+
+    function handleMouseWheel(event) {
+        if (isGadget()) return;
+
+        if (event.detail > 0 || event.wheelDelta < 0) {
+            desceSobe('desce');
+        } else if (event.detail < 0 || event.wheelDelta > 0) {
+            desceSobe('sobe');
+        }
+    }
+
+    document.onkeydown = checkKey;
+
+    /* ANIMA SLIDE AO CLICAR NAS SETAS SOBE/DESCE */
+    function checkKey(e) {
+        e = e || window.event;
+        if (isGadget() === true) return;
+
+        if (e.keyCode == '38') {
+            desceSobe('sobe');
+            preventDefault(e);
+        }
+        else if (e.keyCode == '40') {
+            desceSobe('desce');
+            preventDefault(e);
+        }
+    }
+}
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function enterTop() {
+    isScrolling = true;
+
+    setTimeout(function () {
+        isHome = false;
+        isScrolling = false;
+    }, 1000);
+}
+
+function closeTop() {
+    isScrolling = true;
+
+    setTimeout(function () {
+        isHome = true;
+        isScrolling = false;
+    }, 1000);
+}
+
+/* FAZ SCROLL NO SLIDE */
+function scrollContainer() {
+    var topo = 0;
+    $(".containerSlide").addClass("scroll");
+    newPosition = String(position) + "%";
+
+    $(".slidePage").each(
+        function () {
+            topo = $(this).css('top');
+        }
+    );
+
+    $(".slidePage").css({ 'top': (topo - 20) + '%' });
+
+    isScrolling = true;
+    $(".containerSlide").css("top", newPosition);
+
+    setTimeout(function () {
+        $(".containerSlide").removeClass("scroll");
+    }, 1000);
+
+    setTimeout(function () {
+        isScrolling = false;
+    }, 1400);
+}
+
+/* DESCE E SOBE DOS SLIDES */
+function desceSobe(direcao) {
+    if(direcao==='sobe'){
+        if (position < 0 && isScrolling == false && isHome == false) {
+            position = position + 100;
+            scrollContainer();
+        }
+        if (position == 0 && isScrolling == false && isHome == false) {
+            closeTop();
+        }
+    }
+    
+    if(direcao==='desce'){
+        if (position > -300 && isScrolling == false && isHome == false) {
+            position = position - 100;
+            scrollContainer();
+        }
+        if (position == 0 && isScrolling == false && isHome == true) {
+            enterTop();
+        }
+    }
+}
+/* @end Plugin de Paginacao Slide */
