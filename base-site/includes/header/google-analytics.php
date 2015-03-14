309a46e125194bd319b762586c181a533ffda5af
Brendo Marinho <brendonmm@outlook.com>
2015-03-14 02:26:43 -0300

    Enable Advertiser Features in Google Analytics

--- base-site/includes/header/google-analytics.php
+++ base-site/includes/header/google-analytics.php
@@ -5,6 +5,8 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-XXXXXXXX-1', 'auto');
+        ga('create', 'UA-XXXXXX-XX', 'auto');
+        ga('require', 'displayfeatures');
         ga('send', 'pageview');*/
+
     </script>
