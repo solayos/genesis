4a7617304291d88c302e1e927a2c206fdc868d19
Brendo Marinho <brendonmm@outlook.com>
2016-06-21 02:41:50 -0300

    Reset

--- base-site/includes/header/modules/favicon.php
+++
@@ -1,7 +0,0 @@
-<!-- FAVICON -->
-    <link href="resources/images/favicon.ico" rel="icon" type="image/x-icon" /> <!-- Used by IE, and also by some other browsers if we are not careful -->
-    <link href="resources/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-
-    <link href="resources/images/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" > <!-- The classic favicon, displayed in the tabs -->
-    <link href="resources/images/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" > <!-- For Safari on Mac OS -->
-    <link href="resources/images/favicon-96x96.png" rel="icon" sizes="96x96" type="image/png" > <!-- For Google TV -->
